0d61b5182e328b3c45f91146686f763af4020383
Andrey Smetanin <irqlevel@gmail.com>
2015-01-21 15:10:45 +0300

    correct port parameter

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ bin/ds_ctl dev_add -d /dev/loop0 -f
 bin/ds_ctl dev_add -d /dev/loop1 -f
 bin/ds_ctl dev_add -d /dev/loop2 -f
 iptables -F
-bin/ds_ctl srv_start -s 0.0.0.0 9111
-bin/ds_ctl srv_start -s 0.0.0.0 9112
+bin/ds_ctl srv_start -s 0.0.0.0 -p 9111
+bin/ds_ctl srv_start -s 0.0.0.0 -p 9112
