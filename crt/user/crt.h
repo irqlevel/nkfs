3d582b42b5cf68e241172e80441ffbe52ddae443
Andrey Smetanin <irqlevel@gmail.com>
2015-01-24 14:52:06 +0300

    implemented CRT_BUG / CRT_BUG_ON for user mode crt

--- crt/user/crt.h
+++ crt/user/crt.h
@@ -20,8 +20,15 @@
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 #define EXPORT_SYMBOL(s)
-#define CRT_BUG_ON(cond)
-#define CRT_BUG()
+
+#define CRT_BUG_ON(cond) 			\
+	do {					\
+		if ((cond))			\
+			__builtin_trap();	\
+	} while (0);	
+
+#define CRT_BUG()	\
+	__builtin_trap();
 
 void crt_log_set_path(char *log_path, char *log_name);
 void crt_log_set_level(int level);
